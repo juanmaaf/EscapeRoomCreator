723f1e3469721f88d26d5d3d7fba304d74a52870
juanmaaf <juanmanuel.aneas@wazuh.com>
2025-07-20 21:24:52 +0200

    Creación primer Intent -> Narrativa

--- entrega3/lambda/index.js
+++ entrega3/lambda/index.js
@@ -9,6 +9,22 @@ const LaunchRequestHandler = {
         const speakOutput = '¡Bienvenido a <lang xml:lang="en-US">Escape Room Creator</lang>! Puedes decir: "iniciar juego", para escuchar la narrativa';
         return handlerInput.responseBuilder
             .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+const IniciarJuegoIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'IniciarJuegoIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Narrativa...';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt('¿Qué quieres hacer ahora?')
             .getResponse();
     }
 };
